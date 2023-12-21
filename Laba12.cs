17759354c81f8f51721fcc2b7954322d574facf7
Mac1eer <147583135+Mac1eer@users.noreply.github.com>
2023-12-21 08:31:40 +0300

    Add files via upload

---
+++ Laba12.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите путь к файлу");
+StreamReader pach_user = new StreamReader(Console.ReadLine());
+StreamWriter progect = new StreamWriter(Directory.GetCurrentDirectory(), false);
+progect.Write(pach_user.ReadToEnd);
+pach_user.Close();
+progect.Close();    
+Console.WriteLine("Файл был перезаписан в папку с проектом");
